Fix radius scaling for dataset
Add images to tensorboard

alphapose/utils/logger.py
@@ -2,9 +2,12 @@
 # Copyright (c) Shanghai Jiao Tong University. All rights reserved.
 # Written by Jiefeng Li (jeff.lee.sjtu@gmail.com)
 # -----------------------------------------------------
-
+import cv2
 import torch
 import torch.nn.functional as F
+import numpy as np
+
+from alphapose.utils.transforms import get_max_pred
 
 
 def board_writing(writer, loggers, iterations, dataset='Train'):
@@ -15,16 +18,66 @@ def board_writing(writer, loggers, iterations, dataset='Train'):
             f'{dataset}_avg/{name}', logger.avg, iterations)
 
 
-def debug_writing(writer, outputs, labels, inputs, iterations):
-    tmp_tar = torch.unsqueeze(labels.cpu().data[0], dim=1)
-    # tmp_out = torch.unsqueeze(outputs.cpu().data[0], dim=1)
+def debug_writing(writer, joint_map, joint_radius, joint_map_gt, joint_radius_gt, inputs, iterations,
+                  heatmap_to_coord, bboxes, norm_type, ):
+    batch_index = 10
+    input_map = torch.unsqueeze(joint_map_gt.cpu().data[batch_index], dim=1)
+    output_map = torch.unsqueeze(joint_map.cpu().data[batch_index], dim=1)
 
-    tmp_inp = inputs.cpu().data[0]
-    tmp_inp[0] += 0.406
-    tmp_inp[1] += 0.457
-    tmp_inp[2] += 0.480
+    image = inputs.cpu().data[batch_index]
+    image[0] += 0.406
+    image[1] += 0.457
+    image[2] += 0.480
 
-    tmp_inp[0] += torch.sum(F.interpolate(tmp_tar, scale_factor=4, mode='bilinear'), dim=0)[0]
-    tmp_inp.clamp_(0, 1)
+    image = image.detach()
 
-    writer.add_image('Data/input', tmp_inp, iterations)
+    input_image = 0.5 * image.clone()
+    input_map4 = F.interpolate(input_map, scale_factor=4, mode='bilinear')
+    input_image[0] += 0.5 * torch.sum(input_map4, dim=0)[0]
+    input_image.clamp_(0, 1)
+
+    output_image = 0.5 * image.clone()
+    output_map = F.interpolate(output_map.type(torch.float32), scale_factor=4, mode='bilinear')
+    output_image[0] += 0.5 * torch.sum(output_map, dim=0)[0]
+    output_image.clamp_(0, 1)
+
+    writer.add_image('Data/input_map', input_image, iterations)
+    writer.add_image('Data/output_map', output_image, iterations)
+
+    coords = []
+    for map in input_map:
+        if map[0].max() == 0:
+            coords.append(None)
+            continue
+        c = np.unravel_index(map[0].argmax(), map.shape[1:])
+        coords.append(c)
+
+    keypoints_image = (255 * image.numpy().transpose([1, 2, 0])).astype(np.uint8).copy()
+    for coord, radius in zip(coords, joint_radius_gt[batch_index]):
+        if coord is None:
+            continue
+        if radius < 0:
+            radius = 0
+        center = 4 * coord[1], 4 * coord[0]
+        keypoints_image = cv2.circle(keypoints_image, center, int(radius * image.shape[2]), (255, 0, 0), -1)
+
+    writer.add_image('Data/input_keypoints', keypoints_image.astype(np.float32) / 255, iterations, dataformats="HWC")
+
+    coords = []
+    for map in output_map:
+        if map[0].max() == 0:
+            coords.append(None)
+            continue
+        c = np.unravel_index(map[0].argmax(), map.shape[1:])
+        coords.append(c)
+
+    keypoints_image = (255 * image.numpy().transpose([1, 2, 0])).astype(np.uint8).copy()
+    for coord, radius in zip(coords, joint_radius[batch_index]):
+        if coord is None:
+            continue
+        if radius < 0:
+            radius = 0
+        center = 4 * coord[1], 4 * coord[0]
+        keypoints_image = cv2.circle(keypoints_image, center, int(radius * image.shape[2]), (255, 0, 0), -1)
+
+    writer.add_image('Data/output_keypoints', keypoints_image.astype(np.float32) / 255, iterations, dataformats="HWC")

alphapose/utils/presets/simple_transform.py
@@ -216,11 +216,9 @@ class SimpleTransform(object):
                 center, scale = c_half_body, s_half_body
 
         # rescale
-        scale_factor = 1.0
         if self._train:
             sf = self._scale_factor
-            scale_factor = np.clip(np.random.randn() * sf + 1, 1 - sf, 1 + sf)
-            scale = scale * scale_factor
+            scale = scale * np.clip(np.random.randn() * sf + 1, 1 - sf, 1 + sf)
         else:
             scale = scale * 1.0
 
@@ -260,8 +258,9 @@ class SimpleTransform(object):
             target, target_weight = self._integral_target_generator(joints, self.num_joints, inp_h, inp_w, source)
 
         bbox = _center_scale_to_box(center, scale)
+        distance_coef = cv2.norm(affine_transform([0, 0], trans), affine_transform([0, 1], trans))
         joint_radius = label['radius']
-        joint_radius[joint_radius != -1] *= scale_factor
+        joint_radius[joint_radius != -1] *= distance_coef / inp_w
 
         img = im_to_torch(img)
         img[0].add_(-0.406)

scripts/train.py
@@ -25,7 +25,7 @@ else:
     norm_layer = nn.BatchNorm2d
 
 
-def train(opt, train_loader, m, criterion, optimizer, writer, scaler):
+def train(opt, train_loader, m, criterion, optimizer, writer, scaler, heatmap_to_coord):
     loggers = {
         'joint_loss': DataLogger(),
         'radius_loss': DataLogger(),
@@ -60,7 +60,7 @@ def train(opt, train_loader, m, criterion, optimizer, writer, scaler):
             if cfg.LOSS.get('TYPE') == 'MSELoss':
                 joint_loss = 0.5 * criterion(joint_map.mul(label_masks), labels.mul(label_masks))
                 radius_masks = label_masks[:, :, 0, 0] * (joint_radius_gt != -1)
-                coef = 1e-4
+                coef = 1
                 radius_loss = coef * 0.5 * criterion(joint_radius_gt.mul(radius_masks),
                                               joints_radius.mul(radius_masks))
                 loss = joint_loss + radius_loss
@@ -94,8 +94,10 @@ def train(opt, train_loader, m, criterion, optimizer, writer, scaler):
             board_writing(writer, loggers, opt.trainIters, 'Train')
 
         # Debug
-        if opt.debug and not i % 10:
-            debug_writing(writer, joint_map, labels, inps, opt.trainIters)
+        if opt.debug and not i % 100:
+            debug_writing(
+                writer, joint_map, joints_radius, labels, joint_radius_gt, inps, opt.trainIters,
+                heatmap_to_coord, bboxes, norm_type)
 
         # TQDM
         train_loader.set_description(
@@ -261,7 +263,7 @@ def main():
         logger.info(f'############# Starting Epoch {opt.epoch} | LR: {current_lr} #############')
 
         # Training
-        loggers = train(opt, train_loader, m, criterion, optimizer, writer, scaler)
+        loggers = train(opt, train_loader, m, criterion, optimizer, writer, scaler, heatmap_to_coord)
         logger.info(f'Train-{opt.epoch:d} epoch | '
                     f'{" | ".join(f"{name}:{l.avg:.07f}" for name, l in loggers.items())}')
 